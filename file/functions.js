3a8334fc1689e82f6563609b915746021d13fbe3
augustus-0 <113288678+augustus-0@users.noreply.github.com>
2024-01-24 05:08:01 +0000

    Added years in functions.js

--- file/functions.js
+++ file/functions.js
@@ -40,6 +40,7 @@ $(window).resize(function() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
@@ -55,10 +56,11 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
+	var years = Math.floor(days / 365.25);
 
 //	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 //	$("#clock").html(result);
 
-	var result = days + "</span> Days <span class=\"digit\">" + hours + "</span> Hours <span class=\"digit\">" + minutes + "</span> Minutes <span class=\"digit\">" + seconds + "</span> Seconds"; 
+	var result = years + "</span> Years <span class=\"digit\">" + days + "</span> Days <span class=\"digit\">" + hours + "</span> Hours <span class=\"digit\">" + minutes + "</span> Minutes <span class=\"digit\">" + seconds + "</span> Seconds"; 
 	$("#clock").html(result);
 }
